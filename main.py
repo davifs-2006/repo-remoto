25ee68a3454e996021e6fd84c2a88731891244ee
Davi <davi.fernandes@aluno.impacta.edu.br>
2026-04-12 21:52:07 -0300

    Alteração no main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def read_root():
 
 @app.get("/somar/{a}/{b}")
 def somar(a: int, b: int):
-    # Apenas retorna a soma dos dois números
-    return {"resultado": a + b}
+# ERRO INTENCIONAL AQUI: altere de + para -
+return {"resultado": a - b}
